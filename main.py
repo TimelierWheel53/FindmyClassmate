3ad4966b2634f344c6663ceba5e702c8d8f23ed2
Paul Soriano <School@JOYs-MacBook-Pro.local>
2019-07-26 09:56:47 -0700

    added imports

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #
+import webapp2
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
