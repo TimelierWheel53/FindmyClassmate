c46a5ff66361b207da1862d7a61a2c3891176177
Paul Soriano <School@JOYs-MacBook-Pro.local>
2019-07-30 16:27:22 -0700

    db added and working, passwords hashed."
    done.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import webapp2
 import jinja2
 import os
+import hashlib, binascii #for hashing password
 from google.appengine.ext import ndb
 
 jinja_env = jinja2.Environment(
@@ -9,10 +10,17 @@ jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def hash_password(password):
+    #Hash a password for storing
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
 
 class User(ndb.Model):
-  user_name = ndb.StringProperty(required=False, default= '')
-  password = ndb.StringProperty(required=False, default = '')
+  user_name = ndb.StringProperty(required=False)
+  password = ndb.StringProperty(required=False)
   e_mail = ndb.StringProperty(required=False, default = '')
   grad_date = ndb.StringProperty(required=False, default = '')
   major = ndb.StringProperty(required=False, default = '' )
@@ -22,12 +30,15 @@ class LogIn (webapp2.RequestHandler):
         template=jinja_env.get_template('index.html')
         self.response.write(template.render())
     def post(self):
-        new_user = self.request.get('newname')
-        new_pass = self.request.get('newpass')
-        user = User(user_name = new_user,
-                    password = new_pass)
+        new_user = self.request.get('username')
+        new_pass = self.request.get('password')
+        user = User(user_name = new_user, password = hash_password(new_pass))
         user.put()
-        self.redirect("/profile")
+        user_check = User.query().filter(User.user_name == new_user).fetch()
+        if len(user_check) > 0:
+            self.error(403)
+        else:
+            self.redirect("/profile")
 
 class Profile (webapp2.RequestHandler):
     def get(self):
@@ -38,17 +49,18 @@ class Profile (webapp2.RequestHandler):
         new_mail = self.request.get('email')
         grad = self.request.get('grad')
         new_major = self.request.get('major')
-        user = User(e_mail = new_mail,
-                    grad_date = grad,
-                    major = new_major,)
-        user.put()
+        #get user info from LogIn Class
+        originalUser = User.query().filter(User.user_name == new_name).fetch()[0]
+        originalUser.e_mail = new_mail
+        originalUser.grad_date = grad
+        originalUser.major = new_major
+        originalUser.put()
+
 
 class Major (webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         template=jinja_env.get_template('majors.html')
         self.response.write(template.render())
-
-
 
 application = webapp2.WSGIApplication([
     ('/', LogIn),
